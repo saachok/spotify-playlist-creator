6c595a56afd9242a95df507e5b5e69c7897adec5
Andrii Sachok <andriy.sachok@gmail.com>
2023-02-15 20:09:35 +0200

    added useAuth hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ const code = new URLSearchParams(window.location.search).get('code');
 function App() {
   return (
     <div
-      style={
-        {
-          // display: 'flex',
-          // height: '100vh',
-          // justifyContent: 'center',
-          // alignItems: 'center',
-        }
-      }
+      style={{
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
     >
       {code ? <CreatePlaylistForm code={code} /> : <Login />}
     </div>
