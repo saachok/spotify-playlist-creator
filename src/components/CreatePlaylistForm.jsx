97b84a42d37172b77f261582be08fdb8b96164b3
Andrii Sachok <andriy.sachok@gmail.com>
2023-02-17 22:14:00 +0200

    added track searching

--- src/components/CreatePlaylistForm.jsx
+++ src/components/CreatePlaylistForm.jsx
@@ -2,36 +2,115 @@ import { useState } from 'react';
 
 import { Box, TextField, Button, Typography } from '@mui/material';
 
-import { USER_ID } from '../constants';
+// import { USER_ID } from '../constants';
 import useAuth from '../hooks/useAuth';
 
 const CreatePlaylistForm = ({ code }) => {
   const [playlistTitle, setPlaylistTitle] = useState('');
   const accessToken = useAuth(code);
 
+  const getTrackList = (playlistTitle) => {
+    const tracks = playlistTitle.split(' ');
+    return tracks;
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${USER_ID}/playlists`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          name: playlistTitle,
-          description: 'Test description for playlist',
+    const songsTitles = getTrackList(playlistTitle);
+    console.log(songsTitles);
+    const playlist = [];
+    let searchOffset = 0;
 
-          //FIXME: all playlists create in public mode
-          public: false,
-        }),
+    for (let i = 0; i < songsTitles.length; i++) {
+      let reqSong = `${capitalizeFirstLetter(songsTitles[i])}`;
+
+      if (searchOffset >= 1000) {
+        console.log(`Can't find song with '${reqSong}' title.`);
+        throw new Error('Too big offset!');
       }
-    );
-    const data = await response.json();
-    console.log('data: ', data);
+
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${encodeURI(
+          reqSong
+        )}&type=track&limit=50&offset=${searchOffset}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const data = await response.json();
+      const track = data.tracks.items.find((element) => {
+        return element.name.toLowerCase() === reqSong.toLowerCase();
+      });
+      console.log('track:', track);
+      if (!track) {
+        searchOffset += 50;
+        i--;
+      } else {
+        searchOffset = 0;
+        playlist.push(track);
+      }
+    }
+    console.log(playlist);
+
+    // const promises = songsTitles.map(async (song) => {
+    //   // setReqSongTitle((prevState) => prevState + ' ' + song);
+    //   const response = await fetch(
+    //     `https://api.spotify.com/v1/search?q=${encodeURI(
+    //       // reqSongTitle
+    //       song
+    //     )}&type=track`,
+    //     {
+    //       method: 'GET',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: `Bearer ${accessToken}`,
+    //       },
+    //     }
+    //   );
+    //   const data = await response.json();
+    //   const track = data.tracks.items.find(
+    //     (element) => element.name.toLowerCase() === song.toLowerCase()
+    //   );
+    //   // if (track) {
+    //   // setReqSongTitle('');
+    //   // }
+    //   return track;
+    // });
+
+    // const playlist = await Promise.all(promises);
+    // console.log(playlist);
+
     setPlaylistTitle('');
+
+    // const response = await fetch(
+    //   `https://api.spotify.com/v1/users/${USER_ID}/playlists`,
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       Accept: 'application/json',
+    //       'Content-Type': 'application/json',
+    //       Authorization: `Bearer ${accessToken}`,
+    //     },
+    //     body: JSON.stringify({
+    //       name: playlistTitle,
+    //       description: 'Test description for playlist',
+
+    //       //FIXME: all playlists create in public mode
+    //       public: false,
+    //     }),
+    //   }
+    // );
+    // const data = await response.json();
+    // console.log('data: ', data);
+    // console.log(`Search track: ${playlistTitle}`);
   };
 
   return (
